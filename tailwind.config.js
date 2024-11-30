3d60ebbfbc268cfbea57f62672637573558dfbe5
Ebube Roderick <67063521+ebuberoderick@users.noreply.github.com>
2024-11-30 10:41:46 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+ darkmode:'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
